26a3ae95d61a6974a8cbbf8e54e4ee127c0dfdd0
João Bezerra <joaobzzr@gmail.com>
2023-07-16 23:42:25 -0300

    Change colors

--- src/KonvaTable/defaultTheme.ts
+++ src/KonvaTable/defaultTheme.ts
@@ -3,12 +3,12 @@ import { Theme } from "./types";
 export const defaultTheme: Theme = {
   rowHeight: 30,
   cellPadding: 10,
-  tableBorderColor: "#000000",
+  tableBorderColor: "#665C54",
   scrollBarThickness: 20,
   scrollBarTrackMargin: 2,
-  scrollBarTrackColor: "white",
-  scrollBarThumbColor: "#000000",
+  scrollBarTrackColor: "#282828",
+  scrollBarThumbColor: "#928374",
   fontSize: 14,
-  fontColor: "#000000",
+  fontColor: "#EBDBB2",
   fontFamily: "Arial"
 };
