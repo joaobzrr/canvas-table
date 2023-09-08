c3a63acec1ed492f6f828a120a63998d1477d0d8
João Bezerra <joao.victor@a7participacoes.com>
2023-09-08 15:32:58 -0300

    Rename repository

--- apps/canvas-table-demo/vite.config.ts
+++ apps/canvas-table-demo/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/canvas-table-reprise/",
+  base: "/canvas-table/",
   plugins: [react()],
 });
