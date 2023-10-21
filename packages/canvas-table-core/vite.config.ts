d69d2b3d2f646df8851e81848c694487ca6fe94e
João Bezerra <joaobzzr@gmail.com>
2023-10-21 01:35:14 -0300

    Revert back to single entry

--- packages/canvas-table-core/vite.config.ts
+++ packages/canvas-table-core/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import eslint from "vite-plugin-eslint";
@@ -8,18 +9,10 @@ export default defineConfig({
     eslint()
   ],
   build: {
-    outDir: "dist",
     sourcemap: true,
     lib: {
-      entry: {
-        "canvas-table-core": "src/index.ts",
-        "lib/Stage/index": "./src/lib/Stage/index.ts",
-        "lib/Renderer/index": "./src/lib/Renderer/index.ts",
-        "lib/LineRenderer/index": "./src/lib/LineRenderer/index.ts",
-        "lib/TextRenderer/index": "./src/lib/TextRenderer/index.ts",
-        "lib/GlyphAtlas/index": "./src/lib/GlyphAtlas/index.ts",
-        "lib/UiContext/index": "./src/lib/UiContext/index.ts"
-      },
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "canvas-table-core",
       formats: ["es"]
     }
   }
